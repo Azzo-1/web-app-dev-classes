11e7a18775c32e850d65abe968b5599b8e54c44d
Aarron King <J230612@tafe.wa.edu.au>
2025-08-04 11:24:04 +0800

    Merge

--- session-02/assets/js_starter/scripts_challenge_1.js
+++ session-02/assets/js_starter/scripts_challenge_1.js
@@ -4,8 +4,13 @@ let numbers = [1, 2, 3, 4, 5];
 
 // Use the pop() method to remove the last element from the array
 
+numbers.pop();
+
 // Use the unshift() method to add the last element to the beginning of the array
 
+numbers.unshift(7, 8);
 // Use the slice() method to exclude the first 3 elements and return a new array
+
+let newNumbers = numbers.splice(0, 2);
 
 // Set the value of the output field inner html to the new array
